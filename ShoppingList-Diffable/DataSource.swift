45d5cc582757c5872a1b1579df44d3ff6c82a14d
Juan C <juanceballos@pursuit.org>
2020-07-21 02:48:35 -0400

    reordering using diffable

--- ShoppingList-Diffable/DataSource.swift
+++ ShoppingList-Diffable/DataSource.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 // conforms to UITableViewDataSource
+// snapshot becomes source of truth
 class DataSource: UITableViewDiffableDataSource<Category, Item> {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -39,4 +40,64 @@ class DataSource: UITableViewDiffableDataSource<Category, Item> {
             
         }
     }
+    
+    // possible code snippet reordering diffDSTV
+    // 1. reordering steps
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // 2. reordering steps
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // get the source item
+        guard let sourceItem = itemIdentifier(for: sourceIndexPath) else {return}
+        
+        // Scenario 1: sttempting to move to self
+        guard sourceIndexPath != destinationIndexPath else {return}
+        
+        // get the destination item
+        let destionationItem = itemIdentifier(for: destinationIndexPath)
+        
+        // get the current snapshot
+        var snapshot = self.snapshot()
+        
+        // handle Scenario 2 and 3
+        if let destinationItem = destionationItem {
+           // get the source index and the destiantion index
+            if let sourceIndex = snapshot.indexOfItem(sourceItem),
+                let destinationIndex = snapshot.indexOfItem(destinationItem) {
+                
+                // what order should we be inserting the source item
+                let isAfter = destinationIndex > sourceIndex && snapshot.sectionIdentifier(containingItem: sourceItem) == snapshot.sectionIdentifier(containingItem: destinationItem)
+                
+                // first delete the source item
+                snapshot.deleteItems([sourceItem])
+                // Scenario 2
+                if isAfter  {
+                    snapshot.insertItems([sourceItem], afterItem: destinationItem)
+                }
+                // Scenario 3
+                else {
+                    snapshot.insertItems([sourceItem], beforeItem: destinationItem)
+                }
+            }
+        }
+        
+        // handle Scenario 4
+        // no index path at destination section
+        else {
+            // get the section for the destination idex path
+            let destinationSection = snapshot.sectionIdentifiers[destinationIndexPath.section]
+            
+            // delete the source item before reinserting it
+            snapshot.deleteItems([sourceItem])
+            
+            // append the source item at the new section
+            snapshot.appendItems([sourceItem], toSection: destinationSection)
+        }
+        
+        // apply changes to the data source
+        apply(snapshot, animatingDifferences: false)
+    }
+    
 }
